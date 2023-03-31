7eb90b2af577920f60a2b8b24ae2774b6221e11a
Mahfuz <aamahfuz.pro@gmail.com>
2023-04-01 02:36:50 +0600

    HomeCarat added

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import "./Cart.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 const Cart = (props) => {
     const {pic, name,id, date, profile,title,time} = props.cart;
     return (
         <div className='carts'>  
             <img src={pic} alt="" />
 
-            <div className='d-flex'>
+          
+           
+                
+                <div className='d-flex justify-content-between px-2'>
+                <div className='d-flex'> 
 
-            <div className='profile d-flex gap-2 ms-2'>
-                <img src={profile} alt="" />
-                <div className='d-block'>
-          <h6>{name}</h6>
-          <p className='data'>{date}</p>
-           <p>{time}</p>
-          </div>
-          
-            </div>
-          
-             <h6>{title}</h6>
+                <div className='profile d-flex mx-2'>
+                 <img src={profile} alt="" />
+                   </div>
+                      <div>
+                      <h6>{name}</h6>
+                      <p className='data'>{date}</p>
+                      </div>
+                </div>
+                <div className='d-flex ms-5 align-items-center'>
+                 <p>{time}</p>
+                <button className='btn '><FontAwesomeIcon icon={faBookmark} /></button>
+                </div>
+                
+                </div>
+                <div>
+                   <h6> {title}</h6>
+                   <p>#learner #coder</p>
+                  <p> <a href="#">mark as read</a></p>
+                </div>
+    
+        
              
-             </div>
-             
-         <p>{id}</p>
-      
+            
+     
          <div className='cart'>
       
       </div>
